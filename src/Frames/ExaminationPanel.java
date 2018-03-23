93949e9a6e5f140b064eff212637637335f0626d
abrogow <abrogow@gmail.com>
2018-03-23 09:10:55 +0100

    Ola pracuje

--- src/Frames/ExaminationPanel.java
+++ src/Frames/ExaminationPanel.java
@@ -10,15 +10,5 @@ public class ExaminationPanel extends JPanel
 		setBackground(Color.WHITE);
 		setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 		setBorder(BorderFactory.createTitledBorder("Badanie"));
-		
 	}
-	
-	 /*
-		public  ListFrame(JFrame parentFrame, boolean modal)
-		{
-		 super (parentFrame, modal);// okro modalne
-		 this.setDefaultCloseOperation(DataFrame.DISPOSE_ON_CLOSE);
-		 this.setDefaultCloseOperation(ExaminationFrame.DISPOSE_ON_CLOSE);
-		}*/
-
 }
